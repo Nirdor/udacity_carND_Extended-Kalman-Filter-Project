ca216cef39941a9c2164198c227b1e770fcb78bf
Martin Seidel <seidelmartin@gmx.de>
2018-07-13 23:55:08 +0200

    project complete

---
+++ src/tools.h
@@ -0,0 +1,39 @@
+#ifndef TOOLS_H_
+#define TOOLS_H_
+#include <vector>
+#include "Eigen/Dense"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+using namespace std;
+
+struct radarUpdateData {
+  MatrixXd jacobian;
+  VectorXd hx;
+};
+
+class Tools {
+public:
+  /**
+  * Constructor.
+  */
+  Tools();
+
+  /**
+  * Destructor.
+  */
+  virtual ~Tools();
+
+  /**
+  * A helper method to calculate RMSE.
+  */
+  VectorXd CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth);
+
+  /**
+  * A helper method to calculate Jacobians.
+  */
+  radarUpdateData CalculateUpdateData(const VectorXd& x_state);
+
+};
+
+#endif /* TOOLS_H_ */
